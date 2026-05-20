f5ec7ba6fc04ae7926df06f58da3537f14150e17
glowse <openyourgates@gmail.com>
2026-05-20 12:31:39 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME='spino-ondoku-redesign-v4';
+const CACHE_NAME='spino-ondoku-redesign-v5';
 const PAGE_ASSETS=Array.from({length:8},(_,mi)=>Array.from({length:5},(_,pi)=>`./assets/reader-${mi+1}-${pi+1}.jpg`)).flat();
-const MISSION_ASSETS=Array.from({length:8},(_,i)=>`./assets/mission-${i+1}.svg`);
+const MISSION_ASSETS=Array.from({length:8},(_,i)=>`./assets/mission-${i+1}.jpg`);
 const ASSETS=["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png", "./assets/spino-river.jpg", "./assets/spino-hero.jpg", ...MISSION_ASSETS, ...PAGE_ASSETS];
 
 self.addEventListener('install',event=>{
